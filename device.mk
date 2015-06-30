b1dd0e5f0b6d248087ef04338c602443d4e87d19
Ryuinferno <ryuinferno.xda@gmail.com>
2015-06-30 15:21:30 +0200

    hammerhead: Enable doze
    Kanged from hammerheadcaf
    
    Change-Id: I87ea1c28b834208f4d509714c18579bf1d12bf78

--- device.mk
+++ device.mk
@@ -261,6 +261,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.smgr_mag_cal_en=true \
     ro.qti.sensors.step_detector=true \
     ro.qti.sensors.step_counter=true \
+    ro.qti.sensors.pick_up=false \
     ro.qti.sensors.tap=false \
     ro.qti.sensors.facing=false \
     ro.qti.sensors.tilt=false \
@@ -270,6 +271,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.pedometer=false \
     ro.qti.sensors.pam=false \
     ro.qti.sdk.sensors.gestures=false
+
+# Doze mode
+PRODUCT_PACKAGES += \
+    HammerheadDoze
 
 # Enable some debug messages by default
 PRODUCT_PROPERTY_OVERRIDES += \
