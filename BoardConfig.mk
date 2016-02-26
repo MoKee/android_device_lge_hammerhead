387001a08d53b810c428221432f1f6e2d0f07968
martincz <martincz@mokeedev.com>
2016-02-26 13:03:19 +0800

    hammerhead: mokee bringup
    Change-Id: Ie8439759737288fc87970af7ae73be3f5ac852fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := mokee_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 ifneq ($(filter hammerhead_fp aosp_hammerhead_fp,$(TARGET_PRODUCT)),)
@@ -142,7 +142,7 @@ MALLOC_IMPL := dlmalloc
 endif
 
 # Hardware
-BOARD_HARDWARE_CLASS := device/lge/hammerhead/cmhw
+BOARD_HARDWARE_CLASS := device/lge/hammerhead/mkhw
 
 # Recovery
 RECOVERY_FSTAB_VERSION := 2
